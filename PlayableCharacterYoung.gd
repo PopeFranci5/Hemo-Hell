71a07f7e6057dba7ba3ef12d4b7a4ead8fa569cf
PopeFranci5 <rihardpargma@gmail.com>
2023-04-13 15:13:03 +0300

    added collision

--- PlayableCharacterYoung.gd
+++ PlayableCharacterYoung.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 var speed = 100
 var screen_size
@@ -22,6 +22,7 @@ func _process(delta):
 	elapsed += delta
 	
 	if elapsed > 2.9:
+		@warning_ignore("shadowed_variable_base_class")
 		var velocity = Vector2.ZERO
 		if Input.is_action_pressed("move_up"):
 			velocity.y -= 1
@@ -34,6 +35,8 @@ func _process(delta):
 		if Input.is_action_just_pressed("roll"):
 			velocity = velocity * 2
 			await get_tree().create_timer(3).timeout
+		
+		move_and_collide(Vector2(0, 0))
 		
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
@@ -48,4 +51,3 @@ func _process(delta):
 			$AnimatedSprite2D.animation = "right"
 		elif velocity.x < 0:
 			$AnimatedSprite2D.animation = "left"
-		print(velocity)
